3d6ceba9e14ec519c213f238f151b9f4085f6ab4
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-12-01 02:17:38 +0800

    Change month from 11 to 12

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -34,7 +34,7 @@ where email = 'opacity0@hexschooltest.io'
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 select COUNT(*) as "用戶數量"
-from "USER";
+from "USER"
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 select * 
 from "USER"
