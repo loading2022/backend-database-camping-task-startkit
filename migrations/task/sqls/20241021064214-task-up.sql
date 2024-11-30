007f8275ba4e9d845e895447d6ba26bd7bea7e09
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-12-01 02:42:45 +0800

    Question1

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -22,15 +22,15 @@ VALUES(
   ('好野人', 'richman@hexschooltest.io', 'USER'),
   ('Q太郎', 'starplatinum@hexschooltest.io', 'USER'),
   ('透明人', 'opacity0@hexschooltest.io', 'USER')
-)
+);
 -- 1-2 修改：用 Email 找到 李燕容、肌肉棒子、Q太郎，如果他的 Role 為 USER 將他的 Role 改為 COACH
 update "USER" set "role" = 'COACH'
 where email in ('lee2000@hexschooltest.io', 'muscle@hexschooltest.io', 'starplatinum@hexschooltest.io') 
-and role = 'USER'
+and role = 'USER';
 
 -- 1-3 刪除：刪除USER 資料表中，用 Email 找到透明人，並刪除該筆資料
 delete from "USER"
-where email = 'opacity0@hexschooltest.io'
+where email = 'opacity0@hexschooltest.io';
 
 -- 1-4 查詢：取得USER 資料表目前所有用戶數量（提示：使用count函式）
 select COUNT(*) as "用戶數量"
@@ -38,11 +38,11 @@ from "USER";
 -- 1-5 查詢：取得 USER 資料表所有用戶資料，並列出前 3 筆（提示：使用limit語法）
 select * 
 from "USER"
-limit 3
+limit 3;
 
 
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
 select COUNT(distinct (user_id)) as "預約會員人數" 
 from "COURSE_BOOKING"
-where EXTRACT(MONTH from created_at) = 12 and status = '即將授課'
+where EXTRACT(MONTH from created_at) = 12 and status = '即將授課';
