472bcc0fb70a0efafbd2ac66418a317184c4a5ea
ShihPing0803 <119124535+loading2022@users.noreply.github.com>
2024-12-01 02:53:05 +0800

    task6

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -287,6 +287,46 @@ inner join (
 ) as "COURSE_BOOKING"
 on "COURSE_BOOKING".user_id = "CREDIT_PURCHASE".user_id;
 
+
+-- ████████  █████   █     ███  
+--   █ █   ██    █  █     █     
+--   █ █████ ███ ███      ████  
+--   █ █   █    ██  █     █   █ 
+--   █ █   █████ █   █     ███  
+-- ===================== ====================
+-- 6. 後台報表
+-- 6-1 查詢：查詢專長為重訓的教練，並按經驗年數排序，由資深到資淺（需使用 inner join 與 order by 語法)
+-- 顯示須包含以下欄位： 教練名稱 , 經驗年數, 專長名稱
+select "USER".name as "教練名稱", "COACH".experience_years as "經驗年數", "SKILL".name as "專長名稱"
+from "COACH"
+inner join "USER" on "USER".id = "COACH".user_id 
+inner join "COACH_LINK_SKILL" on "COACH".id = "COACH_LINK_SKILL".coach_id 
+inner join "SKILL" on "SKILL".id = "COACH_LINK_SKILL".skill_id
+order by "COACH".experience_years;
+
+-- 6-2 查詢：查詢每種專長的教練數量，並只列出教練數量最多的專長（需使用 group by, inner join 與 order by 與 limit 語法）
+-- 顯示須包含以下欄位： 專長名稱, coach_total
+select "SKILL".name as "專長名稱", COUNT(*) as "coach_total"
+from "SKILL"
+inner join "COACH_LINK_SKILL" on "SKILL".id = "COACH_LINK_SKILL".skill_id
+group by "SKILL".name
+order by coach_total desc 
+limit 1;
+
+-- 6-3. 查詢：計算 11 月份組合包方案的銷售數量
+-- 顯示須包含以下欄位： 組合包方案名稱, 銷售數量
+select "CREDIT_PACKAGE".name as "組合包方案名稱", COUNT("CREDIT_PACKAGE".id) as "銷售數量"
+from "CREDIT_PACKAGE" 
+inner join "CREDIT_PURCHASE" on "CREDIT_PACKAGE".id = "CREDIT_PURCHASE".credit_package_id
+where EXTRACT(MONTH from "CREDIT_PURCHASE".purchase_at) = 12 
+group by "CREDIT_PACKAGE".name;
+
+-- 6-4. 查詢：計算 11 月份總營收（使用 purchase_at 欄位統計）
+-- 顯示須包含以下欄位： 總營收
+select SUM(price_paid) as "總營收"
+from "CREDIT_PURCHASE"
+where EXTRACT(MONTH from "CREDIT_PURCHASE".purchase_at) = 12;
+
 -- 6-5. 查詢：計算 11 月份有預約課程的會員人數（需使用 Distinct，並用 created_at 和 status 欄位統計）
 -- 顯示須包含以下欄位： 預約會員人數
 select COUNT(distinct (user_id)) as "預約會員人數" 
